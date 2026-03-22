7b4d8f1382e1c9e4d6743d569b6fa14f297b0695
PashaHatsune <39mkui@gmail.com>
2026-03-23 00:44:26 +0200

    не большой рефактор

--- src/userbot/modules/core/exceptions.py
+++ src/userbot/modules/core/exceptions.py
@@ -1,32 +1,21 @@
-
-
-
-class UploadFailed(Exception):
-    pass
-
-class CommandRequiresAdmin(Exception):
-    pass
-
-
-class CommandRequiresOwner(Exception):
-    pass
-
-
-import sys
 from loguru import logger
 
 
+class UploadFailed(Exception): pass
+class CommandRequiresAdmin(Exception): pass
+class CommandRequiresOwner(Exception): pass
 class MatrixBotError(Exception): pass
 class AuthenticationError(MatrixBotError): pass
 class NetworkError(MatrixBotError): pass
 
 
-def handle_error_response(response):
+def handle_error_response(
+        response: int
+) -> None:
     if response.status_code == 401:
         logger.error("Access token is invalid or missing!")
         logger.info("Check your MATRIX_ACCESS_TOKEN.")
-        # Вместо sys.exit(2) лучше выбросить исключение, 
-        # чтобы бот мог "красиво" завершиться или переподключиться
+
         raise AuthenticationError("Invalid token")
         
     elif response.status_code >= 500:
