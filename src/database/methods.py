65370f17a22e832f7cb81b12d42f9a3531c0acd2
PashaHatsune <39mkui@gmail.com>
2026-04-13 14:43:54 +0300

    Добавить: всё настройки модулей хранятся в settings

--- src/database/methods.py
+++ src/database/methods.py
@@ -1,6 +1,3 @@
-
-
-
 from sqlalchemy import select
 
 
@@ -8,20 +5,27 @@ class Database:
     def __init__(self, session_wrapper):
         self._sw = session_wrapper
 
-    async def get(self, owner, key, default=None):
-            async for db in self._sw.get_db():
-                stmt = select(self._sw.Settings).where(
-                    self._sw.Settings.owner == owner,
-                    self._sw.Settings.key == key
-                )
-                result = await db.scalar(stmt)
-                
-                if result is not None:
-                    return result.value
-                
-                return default
+    async def get(self, owner: str, key: str, default=None):
+        """
+        Ищет настройку по имени модуля (owner) и ключу (key).
+        """
+        async for db in self._sw.get_db():
+            stmt = select(self._sw.Settings).where(
+                self._sw.Settings.owner == owner,
+                self._sw.Settings.key == key
+            )
+            result = await db.scalar(stmt)
+            
+            if result is not None:
+                return result.value
+            
+            return default
 
-    async def set(self, owner, key, value):
+    async def set(self, owner: str, key: str, value: any):
+        """
+        Создает или обновляет строку в базе данных.
+        Если это новый модуль или новый ключ - автоматически добавит строку.
+        """
         async for db in self._sw.get_db():
             stmt = select(self._sw.Settings).where(
                 self._sw.Settings.owner == owner,
